24ec040a1202834db31700fb96e87f9ebb99d4ea
ethansauerberg <ethansauerberg@wustl.edu>
2020-04-07 19:43:30 -0700

    asdf

--- scripts.js
+++ scripts.js
@@ -1,20 +1,27 @@
+document.getElementById("main").setAttribute("border", "2px solid red");
 
-// on start up
+
 onLoad();
-//addNew();
+
 function onLoad() {
     sendRequest("GET");
+
 }
 
+
+// need to fix error with only submitting half the time
 var addForm = document.getElementById("addForm");
 addForm.addEventListener("submit", function(){
-   // var toDoName = {text: addForm.};
-   console.log("2");
     var name = addForm.elements["caption"].value;
-    console.log("1");
     sendRequest("POST", { "text": name});
     sendRequest("GET");
 });
+
+//var arrayOfDeletes = 
+
+
+
+
 
 
 function sendRequest(type, data) {
@@ -25,26 +32,11 @@ function sendRequest(type, data) {
             var parsedToDos = JSON.parse(this.responseText);
             console.log(parsedToDos);
             if(type == "GET") {
-                var i = 0;
-                var listOfToDos = document.getElementById("listOfToDos");
-                while(parsedToDos[i] != null) {
-                    var newItem = document.createElement("div");
-                    newItem.innerHTML = "";
-                    newItem.innerHTML += '<div class="toDoItem">';
-                    newItem.innerHTML += '<form><input type="checkbox" class="toDoItemCheckbox"></form>';
-                    newItem.innerHTML += '<span class="toDoItemCaption">' + parsedToDos[i].text + '</span>';
-                    newItem.innerHTML += '<form><input type="button" class="ToDoItemDelete" value="X"></form>';
-                    newItem.innerHTML += '</div>';
-
-                    listOfToDos.appendChild(newItem);
-                    //console.log(parsedToDos[i]);
-                    ++i;
-                    
-                }
+               appendToDos(parsedToDos);
             }
         }
         else if (this.readyState == 4) { //this means this.status isn't 200 (error)
-            console.log(this.responseText);
+            console.log("Error - bad ready state. Response was: " + this.responseText);
         }
     }
     
@@ -57,5 +49,26 @@ function sendRequest(type, data) {
     }
     else if (type == "GET") { 
         xhttp.send();
+    }
+}
+
+
+function appendToDos(parsedToDos) {
+    var i = 0;
+    var listOfToDos = document.getElementById("listOfToDos");
+    while(parsedToDos[i] != null) {
+        var newItem = document.createElement("div");
+        newItem.innerHTML = '<div class="toDoItem"><form><input type="checkbox" class="toDoItemCheckbox">';
+        newItem.innerHTML += '<span class="toDoItemCaption">' + parsedToDos[i].text + '</span>';
+        newItem.innerHTML += '<form><input type="button" class="ToDoItemDelete" value="X"></div>';
+     
+        listOfToDos.appendChild(newItem);
+
+        newItem.setAttribute("display", "inline");
+        newItem.setAttribute("margin", "5px");
+        newItem.setAttribute("vertical-align", "middle");
+        console.log(parsedToDos[i]);
+        ++i;
+        
     }
 }
