6640d6414bd62103bbfb216a6aa5c65cfc4d848d
ethansauerberg <ethansauerberg@wustl.edu>
2020-04-07 19:12:50 -0700

    actually making progress

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 
 // on start up
-sendRequest("GET");
+onLoad();
 //addNew();
-
-function addNew() {
-    sendRequest("POST", { "text": "Another new thing" });
+function onLoad() {
+    sendRequest("GET");
 }
 
+var addForm = document.getElementById("addForm");
+addForm.addEventListener("submit", function(){
+   // var toDoName = {text: addForm.};
+   console.log("2");
+    var name = addForm.elements["caption"].value;
+    console.log("1");
+    sendRequest("POST", { "text": name});
+    sendRequest("GET");
+});
 
 
 function sendRequest(type, data) {
@@ -16,8 +24,26 @@ function sendRequest(type, data) {
         if (this.readyState == 4 && this.status == 200) {
             var parsedToDos = JSON.parse(this.responseText);
             console.log(parsedToDos);
+            if(type == "GET") {
+                var i = 0;
+                var listOfToDos = document.getElementById("listOfToDos");
+                while(parsedToDos[i] != null) {
+                    var newItem = document.createElement("div");
+                    newItem.innerHTML = "";
+                    newItem.innerHTML += '<div class="toDoItem">';
+                    newItem.innerHTML += '<form><input type="checkbox" class="toDoItemCheckbox"></form>';
+                    newItem.innerHTML += '<span class="toDoItemCaption">' + parsedToDos[i].text + '</span>';
+                    newItem.innerHTML += '<form><input type="button" class="ToDoItemDelete" value="X"></form>';
+                    newItem.innerHTML += '</div>';
+
+                    listOfToDos.appendChild(newItem);
+                    //console.log(parsedToDos[i]);
+                    ++i;
+                    
+                }
+            }
         }
-        else if (this.readyState == 4) { //this means this.status isn't 200
+        else if (this.readyState == 4) { //this means this.status isn't 200 (error)
             console.log(this.responseText);
         }
     }
@@ -25,11 +51,11 @@ function sendRequest(type, data) {
     xhttp.open(type, "https://cse204.work/todos", true);
     xhttp.setRequestHeader("x-api-key", "82436b-8c4f08-848b4a-ef5e67-b1bebb");
 
-    if (data != null) { // POST
+    if (type == "POST") {
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send(JSON.stringify(data));
     }
-    else { // GET
+    else if (type == "GET") { 
         xhttp.send();
     }
 }
