74b3c2a2fb8754e3da20cdb9c9408d9427107548
ethansauerberg <ethansauerberg@wustl.edu>
2020-04-10 16:29:26 -0700

    close

--- scripts.js
+++ scripts.js
@@ -1,60 +1,52 @@
-sendRequest("GET");
+refreshList();
 
-function onLoad() {
+function refreshList() {
     sendRequest("GET");
 }
 
 
-var arrayOfDeletes = document.getElementsByClassName("ToDoItemDelete");
-console.log(arrayOfDeletes);
-/* for (var i = 0; i < arrayOfDeletes.length; ++i) {
-    console.log("adding event listener #" + i);
-    arrayOfDeletes[i].addEventListener("click", function () {
-        console.log("delete request called");
-        sendRequest("DELETE", null, arrayOfDeletes[i].id);
-    })
-}*/
-console.log(arrayOfDeletes[1]);
-arrayOfDeletes[1].addEventListener("click", function (event) {
+var submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", function () {
     event.preventDefault();
-    sendRequest("DELETE", null, arrayOfDeletes[1].id);
-})
-
-
-// need to fix error with only submitting half the time
-var addForm = document.getElementById("addForm");
-addForm.addEventListener("submit", function () {
-    var name = addForm.elements["caption"].value;
+    var name = document.getElementById("inputText").value;
     sendRequest("POST", { "text": name });
-    sendRequest("GET");
+    name.value == "";
+    refreshList();
 });
 
 
 
-function sendRequest(type, data, id) {
-    console.log("request of type " + type + " with data " + data);
+function sendRequest(type, data) {
+    console.log("request of type '" + type + "' with data '" + data + "'");
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var parsedToDos = JSON.parse(this.responseText);
-            //console.log(parsedToDos);
+            // console.log(this.responseText);
             if (type == "GET") {
+                var parsedToDos = JSON.parse(this.responseText);
                 appendToDos(parsedToDos);
+                console.log(parsedToDos);
+            }
+            else if (type == "DELETE" || type == "PUT") {
+                console.log("Response text to request of type '" + type + "' with data '" + data + "': " + this.responseText);
+                refreshList();
             }
         }
         else if (this.readyState == 4) { //this means this.status isn't 200 (error)
             console.log("Error - bad ready state. Response was: " + this.responseText);
         }
     }
+
     var url = "https://cse204.work/todos";
-    if(id != null){
-        url += id;
-    }    
+    if (type == "DELETE" || type == "PUT") {
+        url += "/" + event.target.parentNode.id;
+    }
+
     xhttp.open(type, url, true);
     xhttp.setRequestHeader("x-api-key", "82436b-8c4f08-848b4a-ef5e67-b1bebb");
 
-    if (type == "POST") {
+    if (type == "POST" || "PUT") {
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send(JSON.stringify(data));
     }
@@ -63,25 +55,43 @@ function sendRequest(type, data, id) {
     }
 }
 
-
 function appendToDos(parsedToDos) {
     var i = 0;
     var listOfToDos = document.getElementById("listOfToDos");
+    listOfToDos.innerHTML = "";
     while (parsedToDos[i] != null) {
         var toDoItem = document.createElement("div");
-        
-        toDoItem.innerHTML = '<div class="toDoItem"><form><input type="checkbox" class="toDoItemCheckbox">';
-        toDoItem.innerHTML += '<span class="toDoItemCaption">' + parsedToDos[i].text + '</span>';
-        toDoItem.innerHTML += '<button class="ToDoItemDelete">X</button></form></div>';
 
         listOfToDos.appendChild(toDoItem);
 
-        toDoItem.setAttribute("display", "inline");
-        toDoItem.setAttribute("margin", "5px");
-        toDoItem.setAttribute("vertical-align", "middle");
-        toDoItem.setAttribute("border", "2px solid red");
-        document.getElementById("title").setAttribute("border", "2px solid red");
-        ++i;
+        var caption = document.createElement("span");
+        caption.innerText = parsedToDos[i].text;
+        toDoItem.appendChild(caption);
+        var checkButton = document.createElement("button");
+        toDoItem.appendChild(checkButton);
+        var deleteButton = document.createElement("button");
+        toDoItem.appendChild(deleteButton);
 
+        checkButton.innerText = "Mark Done";
+        deleteButton.innerText = "Delete";
+        deleteButton.addEventListener("click", function () {
+            sendRequest('DELETE');
+        });
+        checkButton.addEventListener("click", function () {
+            console.log("sending request to mark item done");
+            sendRequest('PUT', "{'completed':true}");
+        });
+    
+        toDoItem.setAttribute("id", parsedToDos[i].id);
+
+        toDoItem.className = "toDoItem";
+        checkButton.className = "toDoItemCheckbox";
+        deleteButton.className = "toDoItemDelete"
+
+        if(parsedToDos[i].completed == true) {
+            console.log("Found a completed to-do item");
+            caption.setAttribute("text-decoration", "line-through");
+        }
+        ++i;
     }
 }
